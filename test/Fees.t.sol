a88adcd65524ba40db1a26bee5c4853b639edb75
Big Boss <bigboss@metalrodeo.xyz>
2025-02-14 06:02:12 -0600

    chore: fmt

--- test/Fees.t.sol
+++ test/Fees.t.sol
@@ -27,9 +27,7 @@ contract FeesTest is SendEarnTest {
             vm.prank(SUPPLIER);
             morpho.supply(marketParams, MAX_TEST_ASSETS, 0, ONBEHALF, hex"");
 
-            uint256 collateral = uint256(MAX_TEST_ASSETS).wDivUp(
-                marketParams.lltv
-            );
+            uint256 collateral = uint256(MAX_TEST_ASSETS).wDivUp(marketParams.lltv);
             collateralToken.setBalance(BORROWER, collateral);
 
             vm.startPrank(BORROWER);
@@ -66,11 +64,8 @@ contract FeesTest is SendEarnTest {
         uint256 totalAssetsAfter = sevault.totalAssets();
         uint256 interest = totalAssetsAfter - sevault.lastTotalAssets();
         uint256 sendFeeAssets = interest.mulDiv(SEND_FEE, WAD);
-        uint256 feeShares = sendFeeAssets.mulDiv(
-            sevault.totalSupply() + 1,
-            totalAssetsAfter - sendFeeAssets + 1,
-            Math.Rounding.Floor
-        );
+        uint256 feeShares =
+            sendFeeAssets.mulDiv(sevault.totalSupply() + 1, totalAssetsAfter - sendFeeAssets + 1, Math.Rounding.Floor);
         return feeShares;
     }
 
@@ -79,18 +74,10 @@ contract FeesTest is SendEarnTest {
         uint256 interest = totalAssetsAfter - vault.lastTotalAssets();
         uint256 feeAssets = interest.mulDiv(MORPHO_FEE, WAD);
 
-        return
-            feeAssets.mulDiv(
-                vault.totalSupply() + 1,
-                totalAssetsAfter - feeAssets + 1,
-                Math.Rounding.Floor
-            );
+        return feeAssets.mulDiv(vault.totalSupply() + 1, totalAssetsAfter - feeAssets + 1, Math.Rounding.Floor);
     }
 
-    function testAccrueFeeWithinABlock(
-        uint256 deposited,
-        uint256 withdrawn
-    ) public {
+    function testAccrueFeeWithinABlock(uint256 deposited, uint256 withdrawn) public {
         deposited = bound(deposited, MIN_TEST_ASSETS + 1, MAX_TEST_ASSETS);
         // The deposited amount is rounded down on Morpho and thus cannot be withdrawn in a block in most cases
         withdrawn = bound(withdrawn, MIN_TEST_ASSETS, deposited - 1);
@@ -106,19 +93,10 @@ contract FeesTest is SendEarnTest {
         sevault.withdraw(withdrawn, RECEIVER, ONBEHALF);
 
         assertEqLastTotalAssets("lastTotalAssets2");
-        assertApproxEqAbs(
-            sevault.balanceOf(SEND_FEE_RECIPIENT),
-            0,
-            1,
-            "sevault.balanceOf(SEND_FEE_RECIPIENT)"
-        );
+        assertApproxEqAbs(sevault.balanceOf(SEND_FEE_RECIPIENT), 0, 1, "sevault.balanceOf(SEND_FEE_RECIPIENT)");
     }
 
-    function testDepositeAccrueFee(
-        uint256 deposited,
-        uint256 newDeposit,
-        uint256 blocks
-    ) public {
+    function testDepositeAccrueFee(uint256 deposited, uint256 newDeposit, uint256 blocks) public {
         deposited = bound(deposited, MIN_TEST_ASSETS, MAX_TEST_ASSETS);
         newDeposit = bound(newDeposit, MIN_TEST_ASSETS, MAX_TEST_ASSETS);
         blocks = _boundBlocks(blocks);
@@ -145,26 +123,12 @@ contract FeesTest is SendEarnTest {
         sevault.deposit(newDeposit, ONBEHALF);
 
         assertEqLastTotalAssets("lastTotalAssets2");
-        assertEq(
-            sevault.balanceOf(SEND_FEE_RECIPIENT),
-            feeShares,
-            "sevault.balanceOf(SEND_FEE_RECIPIENT)"
-        );
+        assertEq(sevault.balanceOf(SEND_FEE_RECIPIENT), feeShares, "sevault.balanceOf(SEND_FEE_RECIPIENT)");
     }
 
     function assertEqLastTotalAssets(string memory err) internal {
-        assertApproxEqAbs(
-            sevault.lastTotalAssets(),
-            sevault.totalAssets(),
-            1,
-            string.concat("sevault.", err)
-        );
-        assertApproxEqAbs(
-            vault.lastTotalAssets(),
-            vault.totalAssets(),
-            1,
-            string.concat("vault.", err)
-        );
+        assertApproxEqAbs(sevault.lastTotalAssets(), sevault.totalAssets(), 1, string.concat("sevault.", err));
+        assertApproxEqAbs(vault.lastTotalAssets(), vault.totalAssets(), 1, string.concat("vault.", err));
         // after a fee update, there will be a difference in total assets
         // after meta morpho takes their cut.
         // likely there is a way to still make this comparison with
